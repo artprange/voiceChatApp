fd1526c61c7ea8348f0da243c10e0095f5a62250
Arthur Prange <104018176+artprange@users.noreply.github.com>
2024-02-16 08:27:53 -0300

    Revert "Test"

--- igniteTeams/src/components/screens/Groups/index.tsx
+++ igniteTeams/src/components/screens/Groups/index.tsx
@@ -1,19 +1,10 @@
-import {Header} from '../../Header';
-import { Highlight } from '../../HighLight';
-import { GroupCard } from '../../GroupCard';
+import {Header} from '../../Header'
 import  * as S  from './styles';
 
 export function Groups() {
   return (
 	<S.Container>
-    <Header />
-    <Highlight  
-    title="Squads"
-    subTitle= "Jogue com seu squad!"
-    
-    />
-
-    <GroupCard title="Call of Duty"/>
+    <Header/>
 	  
 	</S.Container>
     
